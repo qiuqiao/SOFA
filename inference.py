19fe048b4777b6218fb159e00efe67000d518558
qiuqiao <690697422@qq.com>
2023-10-06 02:24:27 +0800

    inference using dictionary

--- inference.py
+++ inference.py
@@ -8,8 +8,9 @@ import torch
 from model import FullModel
 import yaml
 import pandas as pd
-from tqdm import trange
+from tqdm import trange,tqdm
 from itertools import chain
+import argparse
 
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
@@ -22,11 +23,13 @@ def alignment_decode(ph_seq_num,prob_log,is_edge_prob_log,not_edge_prob_log):
     prob_log=np.array(prob_log.cpu())
     is_edge_prob_log=np.array(is_edge_prob_log.cpu())
     not_edge_prob_log=np.array(not_edge_prob_log.cpu())
+    ph_seq_num=np.array(ph_seq_num)
 
     dp=np.zeros([len(ph_seq_num),prob_log.shape[-1]])-np.inf
     #只能从<EMPTY>开始或者从第一个音素开始
     dp[0,0]=prob_log[ph_seq_num[0],0]
-    dp[1,0]=prob_log[ph_seq_num[1],0]
+    if ph_seq_num[0]==0:
+        dp[1,0]=prob_log[ph_seq_num[1],0]
     backtrack_j=np.zeros_like(dp)-1
     for i in range(1,dp.shape[-1]):
         # [j,i-1]->[j,i]
@@ -35,9 +38,13 @@ def alignment_decode(ph_seq_num,prob_log,is_edge_prob_log,not_edge_prob_log):
         prob2=dp[:-1,i-1]+prob_log[ph_seq_num[1:],i]+is_edge_prob_log[i]-config.inference_empty_punish
         prob2=np.concatenate([np.array([-np.inf]),prob2])
         # [j-2,i-1]->[j,i]
+        # 不能跳过音素，可以跳过<EMPTY>
         prob3=dp[:-2,i-1]+prob_log[ph_seq_num[2:],i]+is_edge_prob_log[i]
+        prob3[ph_seq_num[1:-1]!=0]=-np.inf
         prob3=np.concatenate([np.array([-np.inf,-np.inf]),prob3])
-        prob3[::2]=-np.inf# 不能跳过音素，可以跳过<EMPTY>
+        # print(ph_seq_num[1:-1]!=0)
+        # prob3[::2]=-np.inf
+        # prob3[:]=-np.inf
 
         backtrack_j[:,i]=np.arange(len(prob1))-np.argmax(np.stack([prob1,prob2,prob3],axis=0),axis=0)
         dp[:,i]=np.max(np.stack([prob1,prob2,prob3],axis=0),axis=0)
@@ -45,10 +52,10 @@ def alignment_decode(ph_seq_num,prob_log,is_edge_prob_log,not_edge_prob_log):
     backtrack_j=backtrack_j.astype(np.int32)
     target=[]
     #只能从最后一个音素或者<EMPTY>结束
-    if dp[-1,-1]>=dp[-2,-1]:
-        j=int(len(ph_seq_num)-1)
-    else:
+    if ph_seq_num[-1]==0 and dp[-1,-1]<dp[-2,-1]:
         j=int(len(ph_seq_num)-2)
+    else:
+        j=int(len(ph_seq_num)-1)
     i=int(dp.shape[-1]-1)
     while j>=0:
         target.append(int(ph_seq_num[j]))
@@ -94,11 +101,12 @@ def infer_once(audio_path,ph_seq,model,return_plot=False):
 
         not_edge_prob=1-is_edge_prob
         not_edge_prob_log=torch.log(not_edge_prob)
-    
-    ph_seq_num=[vocab[i] for i in ph_seq if vocab[i] != 0]
-    zeros=[0]*(len(ph_seq_num))
-    ph_seq_num=list(chain.from_iterable(zip(zeros,ph_seq_num)))
-    ph_seq_num.append(0)
+
+    ph_seq_num=[vocab[i] for i in ph_seq]
+    # ph_seq_num=[vocab[i] for i in ph_seq if vocab[i] != 0]
+    # zeros=[0]*(len(ph_seq_num))
+    # ph_seq_num=list(chain.from_iterable(zip(zeros,ph_seq_num)))
+    # ph_seq_num.append(0)
 
     # dynamic programming decoding
     prob_log=seg_prob.log()
@@ -164,38 +172,72 @@ def infer_once(audio_path,ph_seq,model,return_plot=False):
                 utils.plot_spectrogram_and_phonemes(melspec[0],target_pred=frame_confidence*config.n_mels,ph_seq=ph_seq_pred,ph_dur=ph_dur_pred.numpy()),\
                 utils.plot_spectrogram_and_phonemes(seg_prob.cpu(),target_pred=target,target_gt=edge_pred.cpu().numpy()*vocab['<vocab_size>'])
 
+def parse_args():
+    """
+    进行参数的解析
+    """
+    description = "you should add those parameter"
+    parser = argparse.ArgumentParser(description=description)
+    # --model_folder_path
+    # --dictionary_path
+    # --segments_path
+    parser.add_argument('--model_folder_path', type=str, default='ckpt', help='model folder path. It should contain three files: a file with the .pth extension, config.yaml, and vocab.yaml.')
+    parser.add_argument('--dictionary_path', type=str, default=os.path.join('dictionary','opencpop-extension.txt'), help='dictionary path. It should be a txt file.')
+    parser.add_argument('--segments_path', type=str, default='segments', help='segments path. It shoould contain 2 types of file: .wav and .lab')
+
+    return parser.parse_args()
+    
+
 if __name__ == '__main__':
+    args=parse_args()
+
     # load model
     model=FullModel().to(config.device)
-    model_name=config.model_name
-    ckpt_list=os.listdir('ckpt')
-    ckpt_list=[i for i in ckpt_list if i.startswith(model_name)]
-    ckpt_list.sort(key=lambda x:int(x.split('_')[-1].split('.')[0]))
-    ckpt_name=ckpt_list[-1]
-    print(f'loading {ckpt_name}...')
-    model.load_state_dict(torch.load(f'ckpt/{ckpt_name}'))
+    model_path=args.model_folder_path
+    pth_list=os.listdir(model_path)
+    pth_list=[i for i in pth_list if i.endswith('.pth')]
+    if len(pth_list)==0:
+        raise Exception('No .pth file in model folder!')
+    elif len(pth_list)>1:
+        raise Exception('More than one .pth file in model folder!')
+    else:
+        pth_name=pth_list[0]
+    print(f'loading {pth_name}...')
+    model.load_state_dict(torch.load(os.path.join(model_path,pth_name)))
 
+    # load dictionary
+    with open(args.dictionary_path,'r') as f:
+        dictionary=f.readlines()
+    dictionary={i.split('\t')[0].strip():i.split('\t')[1].strip().split(' ') for i in dictionary}
+    
     # inference all data
-    for path, subdirs, files in os.walk(config.inference_data_path):
-        for file in files:
-            if file=='transcriptions.csv':
-                trans=pd.read_csv(os.path.join(path,file))
-                trans['path'] = trans.apply(lambda x: os.path.join(path,'wavs', x['name']+'.wav'), axis=1)
+    for path, subdirs, files in os.walk(args.segments_path):
+        if subdirs==[]:
+            labs=[]
+            wavs=[]
+            for f in files:
+                if f.endswith('.lab'):
+                    labs.append(f)
+                elif f.endswith('.wav'):
+                    wavs.append(f)
 
-                ph_seqs=[]
-                ph_durs=[]
-                ctc_ph_seqs=[]
-                ph_confidences=[]
-                for idx in trange(len(trans)):
-                    ph_seq_pred,ph_dur_pred,ctc_ph_seq,ph_confidence=infer_once(trans.loc[idx,'path'],trans.loc[idx,'ph_seq'].split(' '),model)
-                    ph_seqs.append(' '.join(ph_seq_pred))
-                    ph_durs.append(' '.join([str(i) for i in ph_dur_pred]))
-                    ctc_ph_seqs.append(' '.join(ctc_ph_seq))
-                    ph_confidences.append(ph_confidence)
-                
-                trans['ph_seq']=ph_seqs
-                trans['ph_dur']=ph_durs
-                trans['ctc_ph_seq']=ctc_ph_seqs
-                trans['confidence']=ph_confidences
+            for lab in tqdm(labs):
+                file_name=lab[:-4]
+                if file_name+'.wav' not in wavs:
+                    raise Exception(f'{os.path.join(path,file_name)}.wav not found!')
+                else:
+                    # 根据词典生成音素序列
+                    with open(os.path.join(path,lab),'r') as f:
+                        word_seq=f.readlines()[0].strip().split(' ')
+                    ph_seq_input=[vocab[0]]
+                    for word in word_seq:
+                        if word in dictionary:
+                            ph_seq_input.extend(dictionary[word])
+                            ph_seq_input.append(vocab[0])
+                        else:
+                            raise Exception(f'{word} not in dictionary!')
 
-                trans.to_csv(os.path.join(path,'transcriptions.csv'),index=False)
+                    ph_seq_pred,ph_dur_pred,ctc_ph_seq,ph_confidence=infer_once(os.path.join(path,file_name+'.wav'),ph_seq_input,model)
+
+                    with open(os.path.join(path,file_name+'.TextGrid'),'w') as f:
+                        f.write(' '.join(ph_seq_pred)+'\n'+' '.join([str(i) for i in ph_dur_pred]))
