70790a680545b5652b0402f7070770ae23198c79
qiuqiao <690697422@qq.com>
2023-11-06 21:46:44 +0800

    add auto device detect

--- modules/utils/get_melspec.py
+++ modules/utils/get_melspec.py
@@ -1,11 +1,14 @@
 import modules.rmvpe
+import torch
 
 melspec_transform = None
 
 
 class MelSpecExtractor:
-    def __init__(self, n_mels, sample_rate, win_length, hop_length, fmin, fmax, device):
+    def __init__(self, n_mels, sample_rate, win_length, hop_length, fmin, fmax, device=None):
         global melspec_transform
+        if device is None:
+            device = 'cuda' if torch.cuda.is_available() else 'cpu'
         if melspec_transform is None:
             melspec_transform = modules.rmvpe.MelSpectrogram(
                 n_mel_channels=n_mels,
