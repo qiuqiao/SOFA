3f4512a5159c2ddef365b1975e2f3c9ef3244bf2
qiuqiao <690697422@qq.com>
2023-11-12 19:42:05 +0800

    return wav path list in get_dataset

--- modules/g2p/base_g2p.py
+++ modules/g2p/base_g2p.py
@@ -17,16 +17,18 @@ class BaseG2P:
         ph_seq, word_seq, ph_idx_to_word_idx = self._g2p(text)
         return ph_seq, word_seq, ph_idx_to_word_idx
 
-    def get_dataset(self, wav_path_list):
+    def get_dataset(self, wav_paths):
+        wav_path_list = []
         ph_seq_list = []
         word_seq_list = []
         ph_idx_to_word_idx_list = []
-        for wav_path in wav_path_list:
+        for wav_path in wav_paths:
             if wav_path.with_suffix('.lab').exists():
+                wav_path_list.append(wav_path)
                 with open(wav_path.with_suffix('.lab'), 'r') as f:
                     lab_text = f.read().strip()
                 ph_seq, word_seq, ph_idx_to_word_idx = self(lab_text)
                 ph_seq_list.append(ph_seq)
                 word_seq_list.append(word_seq)
                 ph_idx_to_word_idx_list.append(ph_idx_to_word_idx)
-        return ph_seq_list, word_seq_list, ph_idx_to_word_idx_list
+        return wav_path_list, ph_seq_list, word_seq_list, ph_idx_to_word_idx_list
