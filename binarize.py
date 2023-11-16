c8d8e4d755a1a21eb14fe4cfe6b336d766cfc266
qiuqiao <690697422@qq.com>
2023-11-16 19:02:40 +0800

    tidy up codes

--- binarize.py
+++ binarize.py
@@ -1,31 +1,35 @@
 import pathlib
+
+import click
+import h5py
 import numpy as np
+import pandas as pd
 import torch
 import yaml
 from tqdm import tqdm
-import pandas as pd
-import h5py
-import click
-from modules.utils.load_wav import load_wav
+
 from modules.utils.get_melspec import MelSpecExtractor
+from modules.utils.load_wav import load_wav
 
 
 class ForcedAlignmentBinarizer:
-    def __init__(self,
-                 data_folder,
-                 valid_set_size,
-                 valid_set_preferred_folders,
-                 data_augmentation,
-                 ignored_phonemes,
-                 melspec_config,
-                 max_frame_num,
-                 ):
-
+    def __init__(
+        self,
+        data_folder,
+        valid_set_size,
+        valid_set_preferred_folders,
+        data_augmentation,
+        ignored_phonemes,
+        melspec_config,
+        max_frame_num,
+    ):
         self.data_folder = pathlib.Path(data_folder)
         self.valid_set_size = valid_set_size
         self.valid_set_preferred_folders = valid_set_preferred_folders
         self.data_augmentation = data_augmentation
-        self.data_augmentation["key_shift_choices"] = np.array(self.data_augmentation["key_shift_choices"])
+        self.data_augmentation["key_shift_choices"] = np.array(
+            self.data_augmentation["key_shift_choices"]
+        )
         self.ignored_phonemes = ignored_phonemes
         self.melspec_config = melspec_config
         self.max_frame_num = max_frame_num
@@ -80,7 +84,9 @@ class ForcedAlignmentBinarizer:
             .sort_values(by="preferred", ascending=False)
             .iloc[:valid_set_size, :]
         )
-        meta_data_train = meta_data_df.drop(meta_data_valid.index).reset_index(drop=True)
+        meta_data_train = meta_data_df.drop(meta_data_valid.index).reset_index(
+            drop=True
+        )
         meta_data_valid = meta_data_valid.reset_index(drop=True)
 
         # binarize valid set
@@ -102,12 +108,12 @@ class ForcedAlignmentBinarizer:
         )
 
     def binarize(
-            self,
-            prefix: str,
-            meta_data: pd.DataFrame,
-            vocab: dict,
-            binary_data_folder: str,
-            enable_data_augmentation: bool,
+        self,
+        prefix: str,
+        meta_data: pd.DataFrame,
+        vocab: dict,
+        binary_data_folder: str,
+        enable_data_augmentation: bool,
     ):
         print(f"Binarizing {prefix} set...")
         meta_data["ph_seq"] = meta_data["ph_seq"].apply(
@@ -127,16 +133,17 @@ class ForcedAlignmentBinarizer:
         label_type_to_id = {"no_label": 0, "weak_label": 1, "full_label": 2}
 
         idx = 0
-        total_time = 0.
+        total_time = 0.0
         for _, item in tqdm(meta_data.iterrows(), total=meta_data.shape[0]):
-
             # input_feature: [data_augmentation.size+1,input_dim,T]
             waveform = load_wav(item.wav_path, self.device, self.sample_rate)
             input_feature = self.get_melspec(waveform)
 
             T = input_feature.shape[-1]
             if T > self.max_frame_num:
-                print(f"Item {item.path} has a length of{T * self.max_frame_num} is too long, skip it.")
+                print(
+                    f"Item {item.path} has a length of{T * self.max_frame_num} is too long, skip it."
+                )
                 continue
             else:
                 h5py_item_data = h5py_items.create_group(str(idx))
@@ -147,20 +154,27 @@ class ForcedAlignmentBinarizer:
 
             if enable_data_augmentation:
                 input_features = [input_feature]
-                key_shifts = np.random.choice(self.data_augmentation["key_shift_choices"],
-                                              self.data_augmentation["size"],
-                                              replace=False)
+                key_shifts = np.random.choice(
+                    self.data_augmentation["key_shift_choices"],
+                    self.data_augmentation["size"],
+                    replace=False,
+                )
                 for key_shift in key_shifts:
-                    input_features.append(self.get_melspec(waveform, key_shift=key_shift))
+                    input_features.append(
+                        self.get_melspec(waveform, key_shift=key_shift)
+                    )
 
                 input_feature = torch.stack(input_features, dim=0)
             else:
                 input_feature = input_feature.unsqueeze(0)
 
-            input_feature = ((input_feature - input_feature.mean(dim=[1, 2], keepdim=True))
-                             / input_feature.std(dim=[1, 2], keepdim=True))
+            input_feature = (
+                input_feature - input_feature.mean(dim=[1, 2], keepdim=True)
+            ) / input_feature.std(dim=[1, 2], keepdim=True)
 
-            h5py_item_data["input_feature"] = input_feature.cpu().numpy().astype("float32")
+            h5py_item_data["input_feature"] = (
+                input_feature.cpu().numpy().astype("float32")
+            )
 
             # label_type: []
             label_type_id = label_type_to_id[item.label_type]
@@ -187,7 +201,9 @@ class ForcedAlignmentBinarizer:
             else:
                 ph_seq = np.array(item.ph_seq).astype("int32")
                 ph_dur = np.array(item.ph_dur).astype("float32")
-                ph_time = (np.array(np.concatenate(([0], ph_dur))).cumsum() / self.frame_length)
+                ph_time = (
+                    np.array(np.concatenate(([0], ph_dur))).cumsum() / self.frame_length
+                )
                 ph_interval = np.stack((ph_time[:-1], ph_time[1:]))
 
                 ph_interval = ph_interval[:, ph_seq != 0]
@@ -217,7 +233,7 @@ class ForcedAlignmentBinarizer:
                         st = 0
                     if ed > T:
                         ed = T
-                    ph_frame[int(np.round(st)):int(np.round(ed))] = ph_id
+                    ph_frame[int(np.round(st)) : int(np.round(ed))] = ph_id
 
             h5py_item_data["ph_seq"] = ph_seq.astype("int32")
             h5py_item_data["ph_edge"] = ph_edge.astype("float32")
@@ -233,14 +249,21 @@ class ForcedAlignmentBinarizer:
         for k, v in items_meta_data.items():
             h5py_meta_data[k] = np.array(v)
         h5py_file.close()
-        full_label_ratio = items_meta_data["label_types"].count(2) / len(items_meta_data["label_types"])
-        weak_label_ratio = items_meta_data["label_types"].count(1) / len(items_meta_data["label_types"])
-        no_label_ratio = items_meta_data["label_types"].count(0) / len(items_meta_data["label_types"])
+        full_label_ratio = items_meta_data["label_types"].count(2) / len(
+            items_meta_data["label_types"]
+        )
+        weak_label_ratio = items_meta_data["label_types"].count(1) / len(
+            items_meta_data["label_types"]
+        )
+        no_label_ratio = items_meta_data["label_types"].count(0) / len(
+            items_meta_data["label_types"]
+        )
         print(
             "Data compression ratio: \n"
             f"    full label data: {100 * full_label_ratio:.2f} %,\n"
             f"    weak label data: {100 * weak_label_ratio:.2f} %,\n"
-            f"    no label data: {100 * no_label_ratio:.2f} %.")
+            f"    no label data: {100 * no_label_ratio:.2f} %."
+        )
         print(
             f"Successfully binarized {prefix} set, "
             f"total time {total_time:.2f}s, saved to {h5py_file_path}"
@@ -249,7 +272,8 @@ class ForcedAlignmentBinarizer:
     def get_meta_data(self, data_folder):
         path = data_folder
         trans_path_list = [
-            i for i in path.rglob("transcriptions.csv")
+            i
+            for i in path.rglob("transcriptions.csv")
             if i.name == "transcriptions.csv"
         ]
 
@@ -262,7 +286,13 @@ class ForcedAlignmentBinarizer:
             )
             df["preferred"] = df["wav_path"].apply(
                 lambda path_: (
-                    True if any([i in pathlib.Path(path_).parts for i in self.valid_set_preferred_folders])
+                    True
+                    if any(
+                        [
+                            i in pathlib.Path(path_).parts
+                            for i in self.valid_set_preferred_folders
+                        ]
+                    )
                     else False
                 ),
             )
@@ -289,8 +319,14 @@ class ForcedAlignmentBinarizer:
 
 
 @click.command()
-@click.option("--config_path", "-c", type=str, default="configs/binarize_config.yaml", show_default=True,
-              help="binarize config path")
+@click.option(
+    "--config_path",
+    "-c",
+    type=str,
+    default="configs/binarize_config.yaml",
+    show_default=True,
+    help="binarize config path",
+)
 def binarize(config_path: str):
     with open(config_path, "r") as f:
         config = yaml.safe_load(f)
@@ -298,7 +334,7 @@ def binarize(config_path: str):
     global_config = {
         "max_frame_num": config["max_frame_num"],
         "melspec_config": config["melspec_config"],
-        "data_augmentation_size": config["data_augmentation"]["size"]
+        "data_augmentation_size": config["data_augmentation"]["size"],
     }
     with open(pathlib.Path("data/binary/") / "global_config.yaml", "w") as file:
         yaml.dump(global_config, file)
