497a61d53ec8baa89a5d8f544c65be093f6ae62e
qiuqiao <690697422@qq.com>
2023-11-17 18:06:36 +0800

    update g2p

--- modules/g2p/dictionary_g2p.py
+++ modules/g2p/dictionary_g2p.py
@@ -1,31 +1,47 @@
+import warnings
+
 from modules.g2p.base_g2p import BaseG2P
 
 
 class DictionaryG2P(BaseG2P):
     def __init__(self, **kwargs):
         dict_path = kwargs["dictionary"]
-        with open(dict_path, 'r') as f:
-            dictionary = f.read().strip().split('\n')
-        self.dictionary = {item.split('\t')[0].strip(): item.split('\t')[1].strip().split(' ') for item in dictionary}
+        with open(dict_path, "r") as f:
+            dictionary = f.read().strip().split("\n")
+        self.dictionary = {
+            item.split("\t")[0].strip(): item.split("\t")[1].strip().split(" ")
+            for item in dictionary
+        }
 
     def _g2p(self, input_text):
-        word_seq = input_text.strip().split(' ')
-        ph_seq = ['SP']
+        word_seq = input_text.strip().split(" ")
+        ph_seq = ["SP"]
         ph_idx_to_word_idx = [-1]
         for word_idx, word in enumerate(word_seq):
-            for ph in self.dictionary[word]:
+            phones = self.dictionary[word]
+            for i, ph in enumerate(phones):
+                if (i == 0 or i == len(phones) - 1) and ph == "SP":
+                    warnings.warn(
+                        f"The first or last phoneme of word {word} is SP, which is not allowed. "
+                        "Please check your dictionary."
+                    )
+                    continue
                 ph_seq.append(ph)
                 ph_idx_to_word_idx.append(word_idx)
-            ph_seq.append('SP')
-            ph_idx_to_word_idx.append(-1)
+
+            if ph_seq[-1] != "SP":
+                ph_seq.append("SP")
+                ph_idx_to_word_idx.append(-1)
         return ph_seq, word_seq, ph_idx_to_word_idx
 
 
 if __name__ == "__main__":
     pass
-    # grapheme_to_phoneme = DictionaryG2P('/home/qq/Project/SOFA/dictionary/opencpop-extension.txt')
-    # text = 'wo shi yi ge xue sheng '
-    # ph_seq, word_seq, ph_idx_to_word_idx = grapheme_to_phoneme(text)
-    # print(ph_seq)
-    # print(word_seq)
-    # print(ph_idx_to_word_idx)
+    grapheme_to_phoneme = DictionaryG2P(
+        **{"dictionary": "/home/qq/Project/SOFA/dictionary/opencpop-extension.txt"}
+    )
+    text = "wo SP shi yi ge xue sheng a"
+    ph_seq, word_seq, ph_idx_to_word_idx = grapheme_to_phoneme(text)
+    print(ph_seq)
+    print(word_seq)
+    print(ph_idx_to_word_idx)
--- modules/g2p/phoneme_g2p.py
+++ modules/g2p/phoneme_g2p.py
@@ -6,22 +6,24 @@ class PhonemeG2P(BaseG2P):
         pass
 
     def _g2p(self, input_text):
-        word_seq = input_text.strip().split(' ')
+        word_seq = input_text.strip().split(" ")
+        print(word_seq)
+        word_seq = [ph for ph in word_seq if ph != "SP"]
         ph_seq = ["SP"]
         ph_idx_to_word_idx = [-1]
         for word_idx, word in enumerate(word_seq):
             ph_seq.append(word)
             ph_idx_to_word_idx.append(word_idx)
-            ph_seq.append('SP')
+            ph_seq.append("SP")
             ph_idx_to_word_idx.append(-1)
         return ph_seq, word_seq, ph_idx_to_word_idx
 
 
 if __name__ == "__main__":
     pass
-    # grapheme_to_phoneme = PhonemeG2P()
-    # text = 'wo shi yi ge xue sheng '
-    # ph_seq, word_seq, ph_idx_to_word_idx = grapheme_to_phoneme(text)
-    # print(ph_seq)
-    # print(word_seq)
-    # print(ph_idx_to_word_idx)
+    grapheme_to_phoneme = PhonemeG2P()
+    text = "wo shi yi ge xue sheng SP SP SP"
+    ph_seq, word_seq, ph_idx_to_word_idx = grapheme_to_phoneme(text)
+    print(ph_seq)
+    print(word_seq)
+    print(ph_idx_to_word_idx)
