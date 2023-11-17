50494e53c8f4f8d7a926e7bd8e124033b63abb0e
qiuqiao <690697422@qq.com>
2023-11-17 22:45:35 +0800

    add update RoPE when input seq_len>max_seq_len

--- modules/layer/block/attention.py
+++ modules/layer/block/attention.py
@@ -6,21 +6,21 @@ from einops import rearrange, repeat
 
 class MultiHeadSelfAttention(nn.Module):
     def __init__(
-            self,
-            model_dim: int,
-            num_heads: int,
-            max_seq_len: int = 3200,
-            dropout: float = 0.0,
-            mask: str = "none",
-            init_type: str = "xavier_uniform",
+        self,
+        model_dim: int,
+        num_heads: int,
+        max_seq_len: int = 3200,
+        dropout: float = 0.0,
+        mask: str = "none",
+        init_type: str = "xavier_uniform",
     ):
         super().__init__()
 
         assert num_heads > 0, "num_heads must be positive"
         assert model_dim % num_heads == 0, "model_dim must be divisible by num_heads"
         assert (
-                       model_dim // num_heads
-               ) % 2 == 0, "model_dim // num_heads must be divisible by 2"
+            model_dim // num_heads
+        ) % 2 == 0, "model_dim // num_heads must be divisible by 2"
         assert max_seq_len > 0, "max_seq_len must be positive"
         assert 0.0 <= dropout < 1.0, "dropout must be in range [0.0, 1.0)"
         assert mask in [
@@ -35,6 +35,7 @@ class MultiHeadSelfAttention(nn.Module):
             "kaiming_normal",
         ], "init_type must be one of [xavier_uniform, xavier_normal, kaiming_uniform, kaiming_normal]"
 
+        self.max_seq_len = max_seq_len
         self.model_dim = model_dim
         self.d_k = model_dim // num_heads
         self.num_heads = num_heads
@@ -45,8 +46,10 @@ class MultiHeadSelfAttention(nn.Module):
         self.wv = nn.Linear(model_dim, model_dim)
         self.linear = nn.Linear(model_dim, model_dim)
         self.dropout = nn.Dropout(dropout)
-        self.register_buffer("theta_base", torch.Tensor([10000.0]))  # 常量tensor需要用register_buffer注册，否则.to(device)不起作用
-        cos, sin = self.precompute_rotation_matrix(max_seq_len, self.theta_base)
+        self.register_buffer(
+            "theta_base", torch.Tensor([10000.0])
+        )  # 常量tensor需要用register_buffer注册，否则.to(device)不起作用
+        cos, sin = self.precompute_rotation_matrix(self.max_seq_len, self.theta_base)
         self.register_buffer("rotation_matrix_cos", cos)
         self.register_buffer("rotation_matrix_sin", sin)
 
@@ -72,7 +75,9 @@ class MultiHeadSelfAttention(nn.Module):
         theta_vector = torch.pow(theta_base, power)
         position_vector = torch.arange(seq_len)
         rotation_angle_matrix = torch.outer(position_vector, theta_vector)
-        rotation_angle_matrix = repeat(rotation_angle_matrix, "l d -> l (d repeat)", repeat=2)
+        rotation_angle_matrix = repeat(
+            rotation_angle_matrix, "l d -> l (d repeat)", repeat=2
+        )
         rotation_matrix_cos = torch.cos(rotation_angle_matrix).unsqueeze(0).unsqueeze(0)
         rotation_matrix_sin = torch.sin(rotation_angle_matrix).unsqueeze(0).unsqueeze(0)
         return rotation_matrix_cos, rotation_matrix_sin
@@ -90,17 +95,55 @@ class MultiHeadSelfAttention(nn.Module):
         # print(xq.shape, self.rotation_matrix_cos.shape)
         xq_ = get_sin_weight(xq)
         xk_ = get_sin_weight(xk)
-        xq_out = (xq * self.rotation_matrix_cos[:, :, xq.shape[2], :, ] +
-                  xq_ * self.rotation_matrix_sin[:, :, xq.shape[2], :, ])
-        xk_out = (xk * self.rotation_matrix_cos[:, :, xk.shape[2], :, ] +
-                  xk_ * self.rotation_matrix_sin[:, :, xk.shape[2], :, ])
+        xq_out = (
+            xq
+            * self.rotation_matrix_cos[
+                :,
+                :,
+                xq.shape[2],
+                :,
+            ]
+            + xq_
+            * self.rotation_matrix_sin[
+                :,
+                :,
+                xq.shape[2],
+                :,
+            ]
+        )
+        xk_out = (
+            xk
+            * self.rotation_matrix_cos[
+                :,
+                :,
+                xk.shape[2],
+                :,
+            ]
+            + xk_
+            * self.rotation_matrix_sin[
+                :,
+                :,
+                xk.shape[2],
+                :,
+            ]
+        )
 
         return xq_out, xk_out
+
+    def _update_RoPE(self, seq_len):
+        cos, sin = self.precompute_rotation_matrix(seq_len, self.theta_base)
+        self.cos = cos
+        self.sin = sin
+        self.max_seq_len = seq_len
 
     def forward(self, x: torch.Tensor):  # , lengths=None
         # input: Tensor[batch_size seq_len, hidden_dims], lengths: Tensor[batch_size]
         # output: Tensor[batch_size seq_len, hidden_dims]
         batch_size, seq_len, _ = x.shape
+
+        if self.max_seq_len < seq_len:
+            self._update_RoPE(seq_len)
+
         xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
         xq = xq.view(batch_size, self.num_heads, seq_len, self.d_k)
         xk = xk.view(batch_size, self.num_heads, seq_len, self.d_k)
