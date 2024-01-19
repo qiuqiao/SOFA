1f903012d0648777fe045aa183d46117dc4eb3df
qiuqiao <690697422@qq.com>
2024-01-19 12:30:17 +0800

    add assert and comments

--- modules/AP_detector/base_detector.py
+++ modules/AP_detector/base_detector.py
@@ -6,6 +6,8 @@ class BaseAPDetector:
     def process(self, predictions):
         # input: list of predictions, each prediction is a tuple of:
         #     wav_path: pathlib.Path
+        #     wav_length: float
+        #     confidence: float
         #     ph_seq: list of phonemes, SP is the silence phoneme.
         #     ph_intervals: np.ndarray of shape (n_ph, 2), ph_intervals[i] = [start, end]
         #                   means the i-th phoneme starts at start and ends at end.
@@ -24,17 +26,17 @@ class BaseAPDetector:
             word_seq,
             word_intervals,
         ) in predictions:
-            res.append(
-                self._process_one(
-                    wav_path,
-                    wav_length,
-                    confidence,
-                    ph_seq,
-                    ph_intervals,
-                    word_seq,
-                    word_intervals,
-                )
+            prediction = self._process_one(
+                wav_path,
+                wav_length,
+                confidence,
+                ph_seq,
+                ph_intervals,
+                word_seq,
+                word_intervals,
             )
+            assert len(prediction) == 7
+            res.append(prediction)
 
         return res
 
