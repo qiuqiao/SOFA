ddbb5a8c62271ffbd5fe5adc60e45acaba990098
qiuqiao <690697422@qq.com>
2024-04-03 17:00:38 +0800

    refactor: extract  loss calculation logic into a class

---
+++ modules/loss/forced_alignment_loss.py
@@ -0,0 +1,313 @@
+import torch
+from einops import rearrange, repeat
+from torch import nn
+
+import modules.scheduler as scheduler_module
+from modules.loss.binary_emd_loss import BinaryEMDLoss
+from modules.loss.ghm_Loss import CTCGHMLoss, GHMLoss, MultiLabelGHMLoss
+
+
+class ForcedAlignmentLoss(nn.Module):
+
+    def __init__(self, loss_config, optimizer_config, vocab, data_augmentation_enabled):
+        super(ForcedAlignmentLoss, self).__init__()
+        self.device = None
+        self.data_augmentation_enabled = data_augmentation_enabled
+        self.vocab = vocab
+        self.losses_names = [
+            "ph_frame_GHM_loss",
+            "ph_edge_GHM_loss",
+            "ph_edge_EMD_loss",
+            "ph_edge_diff_loss",
+            "ctc_GHM_loss",
+            "consistency_loss",
+            "pseudo_label_loss",
+            "total_loss",
+        ]
+        self.losses_weights = torch.tensor(loss_config["losses"]["weights"])
+
+        self.losses_schedulers = []
+        for enabled in loss_config["losses"]["enable_RampUpScheduler"]:
+            if enabled:
+                self.losses_schedulers.append(
+                    scheduler_module.GaussianRampUpScheduler(
+                        max_steps=optimizer_config["total_steps"]
+                    )
+                )
+            else:
+                self.losses_schedulers.append(scheduler_module.NoneScheduler())
+
+        self.ph_frame_GHM_loss_fn = GHMLoss(
+            self.vocab["<vocab_size>"],
+            loss_config["function"]["num_bins"],
+            loss_config["function"]["alpha"],
+            loss_config["function"]["label_smoothing"],
+        )
+        self.pseudo_label_GHM_loss_fn = MultiLabelGHMLoss(
+            self.vocab["<vocab_size>"],
+            loss_config["function"]["num_bins"],
+            loss_config["function"]["alpha"],
+            loss_config["function"]["label_smoothing"],
+        )
+        self.ph_edge_GHM_loss_fn = MultiLabelGHMLoss(
+            1,
+            loss_config["function"]["num_bins"],
+            loss_config["function"]["alpha"],
+            label_smoothing=0.0,
+        )
+        self.EMD_loss_fn = BinaryEMDLoss()
+        self.ph_edge_diff_GHM_loss_fn = MultiLabelGHMLoss(
+            1,
+            loss_config["function"]["num_bins"],
+            loss_config["function"]["alpha"],
+            label_smoothing=0.0,
+        )
+        self.MSE_loss_fn = nn.MSELoss()
+        self.CTC_GHM_loss_fn = CTCGHMLoss(alpha=1 - 1e-3)
+
+    def forward(self, pred, target):
+        raise NotImplementedError
+
+    def _losses_schedulers_step(self):
+        for scheduler in self.losses_schedulers:
+            scheduler.step()
+
+    def _losses_schedulers_call(self):
+        return torch.tensor([scheduler() for scheduler in self.losses_schedulers]).to(
+            self.device
+        )
+
+    def _get_loss(
+        self,
+        ph_frame_logits,  # (B, T, vocab_size)
+        ph_edge_logits,  # (B, T)
+        ctc_logits,  # (B, T, vocab_size)
+        ph_frame_gt,  # (B, T)
+        ph_edge_gt,  # (B, T)
+        ph_seq_gt,  # (B S)
+        ph_seq_lengths_gt,  # (B)
+        ph_mask,  # (B vocab_size)
+        input_feature_lengths,  # (B)
+        label_type,  # (B)
+        valid=False,
+    ):
+        full_label_idx = label_type >= 2
+        weak_label_idx = label_type >= 1
+        not_full_label_idx = label_type < 2
+        ZERO = torch.tensor(0).to(self.device)
+
+        if (full_label_idx).any():
+            (
+                ph_frame_GHM_loss,
+                ph_edge_GHM_loss,
+                ph_edge_EMD_loss,
+                ph_edge_diff_loss,
+            ) = self._get_full_label_loss(
+                ph_frame_logits[full_label_idx, :, :],
+                ph_edge_logits[full_label_idx, :],
+                ph_frame_gt[full_label_idx, :],
+                ph_edge_gt[full_label_idx, :],
+                input_feature_lengths[full_label_idx],
+                ph_mask[full_label_idx, :],
+                valid,
+            )
+        else:
+            ph_frame_GHM_loss = ph_edge_GHM_loss = ZERO
+            ph_edge_EMD_loss = ph_edge_diff_loss = ZERO
+
+        # TODO:这种pack方式无法处理只有batch中的一部分需要计算Loss的情况，改掉
+        if (weak_label_idx).any():
+            ctc_GHM_loss = ZERO
+            ctc_GHM_loss = self._get_weak_label_loss(
+                ctc_logits[weak_label_idx, :, :],
+                ph_mask[weak_label_idx, :],
+                ph_seq_gt[weak_label_idx, :],
+                ph_seq_lengths_gt[weak_label_idx],
+                input_feature_lengths[weak_label_idx],
+                valid,
+            )
+        else:
+            ctc_GHM_loss = ZERO
+
+        if not valid and self.data_augmentation_enabled:
+            consistency_loss = self._get_consistency_loss(
+                ph_frame_logits, ph_edge_logits, input_feature_lengths
+            )
+            pseudo_label_loss = ZERO
+            # pseudo_label_loss = self._get_pseudo_label_loss(
+            #     ph_frame_logits[not_full_label_idx, :, :],
+            #     input_feature_lengths[not_full_label_idx],
+            #     valid,
+            # )
+        else:
+            consistency_loss = ZERO
+            pseudo_label_loss = ZERO
+
+        losses = [
+            ph_frame_GHM_loss,
+            ph_edge_GHM_loss,
+            ph_edge_EMD_loss,
+            ph_edge_diff_loss,
+            ctc_GHM_loss,
+            consistency_loss,
+            pseudo_label_loss,
+        ]
+
+        return losses
+
+    def _get_full_label_loss(
+        self,
+        ph_frame_logits,
+        ph_edge_logits,
+        ph_frame_gt,
+        ph_edge_gt,
+        input_feature_lengths,
+        ph_mask,
+        valid,
+    ):
+        T = ph_frame_logits.shape[1]
+
+        # ph_frame_prob_gt = nn.functional.one_hot(
+        #     ph_frame_gt.long(), num_classes=self.vocab["<vocab_size>"]
+        # ).float()
+
+        # calculate mask matrix
+        # (B, T)
+        mask = torch.arange(T).to(self.device)
+        mask = repeat(mask, "T -> B T", B=ph_frame_logits.shape[0])
+        mask = (mask < input_feature_lengths.unsqueeze(1)).to(ph_frame_logits.dtype)
+
+        # ph_frame_loss
+        # print((mask.unsqueeze(-1) * ph_mask.unsqueeze(1)).shape, ph_frame_pred.shape)
+        ph_frame_GHM_loss = self.ph_frame_GHM_loss_fn(
+            ph_frame_logits,
+            ph_frame_gt,
+            (mask.unsqueeze(-1) * ph_mask.unsqueeze(1)),
+            valid,
+        )
+
+        # ph_edge loss
+        # BCE_GHM loss
+        ph_edge_GHM_loss = self.ph_edge_GHM_loss_fn(
+            ph_edge_logits.unsqueeze(-1), ph_edge_gt.unsqueeze(-1), mask, valid
+        )
+
+        # EMD loss
+        ph_edge_pred = torch.nn.functional.sigmoid(ph_edge_logits.float())
+        ph_edge_EMD_loss = self.EMD_loss_fn(ph_edge_pred * mask, ph_edge_gt * mask)
+
+        # diff loss
+        ph_edge_diff_loss = self.ph_edge_diff_GHM_loss_fn(
+            (torch.diff(ph_edge_logits, 1, dim=-1) + 1).unsqueeze(-1) / 2,
+            (torch.diff(ph_edge_gt, 1, dim=-1) + 1).unsqueeze(-1) / 2,
+            mask[:, 1:],
+            valid,
+        )
+        return ph_frame_GHM_loss, ph_edge_GHM_loss, ph_edge_EMD_loss, ph_edge_diff_loss
+
+    def _get_weak_label_loss(
+        self,
+        ctc_logits,
+        ph_mask,
+        ph_seq_gt,
+        ph_seq_lengths_gt,
+        input_feature_lengths,
+        valid,
+    ):
+        ctc_logits = ctc_logits - ph_mask.unsqueeze(1).logical_not().float() * 1e9
+        log_probs_pred = nn.functional.log_softmax(ctc_logits, dim=-1)
+        # ctc loss
+        log_probs_pred = rearrange(log_probs_pred, "B T C -> T B C")
+        ctc_GHM_loss = self.CTC_GHM_loss_fn(
+            log_probs_pred,
+            ph_seq_gt,
+            input_feature_lengths,
+            ph_seq_lengths_gt,
+            valid,
+        )
+
+        return ctc_GHM_loss
+
+    def _get_consistency_loss(
+        self, ph_frame_logits, ph_edge_logits, input_feature_lengths
+    ):
+        output_tensors = torch.cat(
+            [ph_frame_logits, ph_edge_logits.unsqueeze(-1)], dim=-1
+        )
+        output_tensors = torch.nn.functional.sigmoid(output_tensors.float())
+        B = output_tensors.shape[0]
+        T = output_tensors.shape[1]
+
+        # calculate mask matrix
+        # (B//2, T, 1)
+        mask = torch.arange(T).to(self.device)
+        mask = repeat(mask, "T -> B T", B=B // 2)
+        mask = (
+            (mask < input_feature_lengths[: B // 2].unsqueeze(1))
+            .to(torch.bool)
+            .unsqueeze(-1)
+        )
+
+        # consistency loss
+        consistency_loss = self.MSE_loss_fn(
+            output_tensors[: B // 2, :, :] * mask,
+            output_tensors[B // 2 :, :, :] * mask,
+        )
+
+        return consistency_loss
+
+    def _get_pseudo_label_loss(self, ph_frame_logits, input_feature_lengths, valid):
+        B = ph_frame_logits.shape[0]
+        T = ph_frame_logits.shape[1]
+
+        ph_edge_prob = torch.nn.functional.sigmoid(ph_frame_logits.float())
+
+        pred1 = ph_edge_prob[: B // 2, :]
+        pred2 = ph_edge_prob[B // 2 :, :]
+        pseudo_label1 = (pred1 >= 0.5).float()
+        pseudo_label2 = (pred2 >= 0.5).float()
+        gradient_magnitude1 = torch.abs(pred1 - pseudo_label1)
+        gradient_magnitude2 = torch.abs(pred2 - pseudo_label2)
+        gradient_magnitude = (gradient_magnitude1 + gradient_magnitude2) / 2
+
+        # calculate mask matrix
+        # (B//2, T, 1)
+        mask = torch.arange(T).to(self.device)
+        mask = repeat(mask, "T -> B T", B=B // 2)
+        mask = (
+            (mask < input_feature_lengths[: B // 2].unsqueeze(1))
+            .to(torch.bool)
+            .unsqueeze(-1)
+        )
+        pseudo_label_mask = (  # (B//2, T)
+            mask
+            & (pseudo_label1 == pseudo_label2)
+            & (gradient_magnitude < self.pseudo_label_auto_theshold)
+        )
+
+        if pseudo_label_mask.sum() / mask.sum() < self.pseudo_label_ratio:
+            self.pseudo_label_auto_theshold += 0.005
+        else:
+            self.pseudo_label_auto_theshold -= 0.005
+
+        if pseudo_label_mask.any():
+            pseudo_label_loss = self.pseudo_label_GHM_loss_fn(
+                ph_frame_logits,
+                torch.cat([pseudo_label1, pseudo_label2], dim=0),
+                torch.cat([pseudo_label_mask, pseudo_label_mask], dim=0),
+                valid,
+            )
+        else:
+            pseudo_label_loss = torch.tensor(0).to(self.device)
+
+        return pseudo_label_loss
+
+    def resume(self, global_step):
+        for scheduler in self.losses_schedulers:
+            scheduler.resume(global_step)
+
+    def to(self, device):
+        self.device = device
+        self.losses_weights = self.losses_weights.to(device)
+        super(ForcedAlignmentLoss, self).to(device)
+        return self
--- modules/task/forced_alignment.py
+++ modules/task/forced_alignment.py
@@ -6,14 +6,12 @@ import torch
 import torch.nn as nn
 import torch.optim.lr_scheduler as lr_scheduler_module
 import yaml
-from einops import rearrange, repeat
+from einops import repeat
 
-import modules.scheduler as scheduler_module
 from modules.layer.backbone.unet import UNetBackbone
 from modules.layer.block.resnet_block import ResidualBasicBlock
 from modules.layer.scaling.stride_conv import DownSampling, UpSampling
-from modules.loss.binary_emd_loss import BinaryEMDLoss
-from modules.loss.ghm_Loss import CTCGHMLoss, GHMLoss, MultiLabelGHMLoss
+from modules.loss.forced_alignment_loss import ForcedAlignmentLoss
 from modules.utils.get_melspec import MelSpecExtractor
 from modules.utils.load_wav import load_wav
 from modules.utils.plot import plot_for_valid
@@ -54,8 +52,6 @@ class LitForcedAlignmentTask(pl.LightningModule):
         self.pseudo_label_ratio = loss_config["function"]["pseudo_label_ratio"]
         self.pseudo_label_auto_theshold = 0.5
 
-        self.data_augmentation_enabled = data_augmentation_enabled
-
         # loss function
         self.losses_names = [
             "ph_frame_GHM_loss",
@@ -67,46 +63,9 @@ class LitForcedAlignmentTask(pl.LightningModule):
             "pseudo_label_loss",
             "total_loss",
         ]
-        self.losses_weights = torch.tensor(loss_config["losses"]["weights"])
-
-        self.losses_schedulers = []
-        for enabled in loss_config["losses"]["enable_RampUpScheduler"]:
-            if enabled:
-                self.losses_schedulers.append(
-                    scheduler_module.GaussianRampUpScheduler(
-                        max_steps=optimizer_config["total_steps"]
-                    )
-                )
-            else:
-                self.losses_schedulers.append(scheduler_module.NoneScheduler())
-
-        self.ph_frame_GHM_loss_fn = GHMLoss(
-            self.vocab["<vocab_size>"],
-            loss_config["function"]["num_bins"],
-            loss_config["function"]["alpha"],
-            loss_config["function"]["label_smoothing"],
+        self.loss = ForcedAlignmentLoss(
+            loss_config, optimizer_config, self.vocab, data_augmentation_enabled
         )
-        self.pseudo_label_GHM_loss_fn = MultiLabelGHMLoss(
-            self.vocab["<vocab_size>"],
-            loss_config["function"]["num_bins"],
-            loss_config["function"]["alpha"],
-            loss_config["function"]["label_smoothing"],
-        )
-        self.ph_edge_GHM_loss_fn = MultiLabelGHMLoss(
-            1,
-            loss_config["function"]["num_bins"],
-            loss_config["function"]["alpha"],
-            label_smoothing=0.0,
-        )
-        self.EMD_loss_fn = BinaryEMDLoss()
-        self.ph_edge_diff_GHM_loss_fn = MultiLabelGHMLoss(
-            1,
-            loss_config["function"]["num_bins"],
-            loss_config["function"]["alpha"],
-            label_smoothing=0.0,
-        )
-        self.MSE_loss_fn = nn.MSELoss()
-        self.CTC_GHM_loss_fn = CTCGHMLoss(alpha=1 - 1e-3)
 
         # validation_step_outputs
         self.validation_step_outputs = {"losses": []}
@@ -156,10 +115,8 @@ class LitForcedAlignmentTask(pl.LightningModule):
         return {"optimizer": optimizer, "lr_scheduler": scheduler}
 
     def on_train_start(self):
-        # resume loss schedulers
-        for scheduler in self.losses_schedulers:
-            scheduler.resume(self.global_step)
-        self.losses_weights = self.losses_weights.to(self.device)
+        self.loss.resume(self.global_step)
+        self.loss.to(self.device)
 
     def training_step(self, batch, batch_idx):
         try:
@@ -180,7 +137,7 @@ class LitForcedAlignmentTask(pl.LightningModule):
                 ctc_logits,  # (B, T, vocab_size)
             ) = self.forward(input_feature.transpose(1, 2))
 
-            losses = self._get_loss(
+            losses = self.loss._get_loss(
                 ph_frame_logits,
                 ph_edge_logits,
                 ctc_logits,
@@ -194,10 +151,10 @@ class LitForcedAlignmentTask(pl.LightningModule):
                 valid=False,
             )
 
-            schedule_weight = self._losses_schedulers_call()
-            self._losses_schedulers_step()
+            schedule_weight = self.loss._losses_schedulers_call()
+            self.loss._losses_schedulers_step()
             total_loss = (
-                torch.stack(losses) * self.losses_weights * schedule_weight
+                torch.stack(losses) * self.loss.losses_weights * schedule_weight
             ).sum()
             losses.append(total_loss)
 
@@ -227,240 +184,6 @@ class LitForcedAlignmentTask(pl.LightningModule):
         ph_edge_logits = logits[:, :, 0]
         ctc_logits = torch.cat([logits[:, :, [1]], logits[:, :, 3:]], dim=-1)
         return ph_frame_logits, ph_edge_logits, ctc_logits
-
-    def _losses_schedulers_step(self):
-        for scheduler in self.losses_schedulers:
-            scheduler.step()
-
-    def _losses_schedulers_call(self):
-        return torch.tensor([scheduler() for scheduler in self.losses_schedulers]).to(
-            self.device
-        )
-
-    def _get_loss(
-        self,
-        ph_frame_logits,  # (B, T, vocab_size)
-        ph_edge_logits,  # (B, T)
-        ctc_logits,  # (B, T, vocab_size)
-        ph_frame_gt,  # (B, T)
-        ph_edge_gt,  # (B, T)
-        ph_seq_gt,  # (B S)
-        ph_seq_lengths_gt,  # (B)
-        ph_mask,  # (B vocab_size)
-        input_feature_lengths,  # (B)
-        label_type,  # (B)
-        valid=False,
-    ):
-        full_label_idx = label_type >= 2
-        weak_label_idx = label_type >= 1
-        not_full_label_idx = label_type < 2
-        ZERO = torch.tensor(0).to(self.device)
-
-        if (full_label_idx).any():
-            (
-                ph_frame_GHM_loss,
-                ph_edge_GHM_loss,
-                ph_edge_EMD_loss,
-                ph_edge_diff_loss,
-            ) = self._get_full_label_loss(
-                ph_frame_logits[full_label_idx, :, :],
-                ph_edge_logits[full_label_idx, :],
-                ph_frame_gt[full_label_idx, :],
-                ph_edge_gt[full_label_idx, :],
-                input_feature_lengths[full_label_idx],
-                ph_mask[full_label_idx, :],
-                valid,
-            )
-        else:
-            ph_frame_GHM_loss = ph_edge_GHM_loss = ZERO
-            ph_edge_EMD_loss = ph_edge_diff_loss = ZERO
-
-        # TODO:这种pack方式无法处理只有batch中的一部分需要计算Loss的情况，改掉
-        if (weak_label_idx).any():
-            ctc_GHM_loss = ZERO
-            ctc_GHM_loss = self._get_weak_label_loss(
-                ctc_logits[weak_label_idx, :, :],
-                ph_mask[weak_label_idx, :],
-                ph_seq_gt[weak_label_idx, :],
-                ph_seq_lengths_gt[weak_label_idx],
-                input_feature_lengths[weak_label_idx],
-                valid,
-            )
-        else:
-            ctc_GHM_loss = ZERO
-
-        if not valid and self.data_augmentation_enabled:
-            consistency_loss = self._get_consistency_loss(
-                ph_frame_logits, ph_edge_logits, input_feature_lengths
-            )
-            pseudo_label_loss = ZERO
-            # pseudo_label_loss = self._get_pseudo_label_loss(
-            #     ph_frame_logits[not_full_label_idx, :, :],
-            #     input_feature_lengths[not_full_label_idx],
-            #     valid,
-            # )
-        else:
-            consistency_loss = ZERO
-            pseudo_label_loss = ZERO
-
-        losses = [
-            ph_frame_GHM_loss,
-            ph_edge_GHM_loss,
-            ph_edge_EMD_loss,
-            ph_edge_diff_loss,
-            ctc_GHM_loss,
-            consistency_loss,
-            pseudo_label_loss,
-        ]
-
-        return losses
-
-    def _get_full_label_loss(
-        self,
-        ph_frame_logits,
-        ph_edge_logits,
-        ph_frame_gt,
-        ph_edge_gt,
-        input_feature_lengths,
-        ph_mask,
-        valid,
-    ):
-        T = ph_frame_logits.shape[1]
-
-        # ph_frame_prob_gt = nn.functional.one_hot(
-        #     ph_frame_gt.long(), num_classes=self.vocab["<vocab_size>"]
-        # ).float()
-
-        # calculate mask matrix
-        # (B, T)
-        mask = torch.arange(T).to(self.device)
-        mask = repeat(mask, "T -> B T", B=ph_frame_logits.shape[0])
-        mask = (mask < input_feature_lengths.unsqueeze(1)).to(ph_frame_logits.dtype)
-
-        # ph_frame_loss
-        # print((mask.unsqueeze(-1) * ph_mask.unsqueeze(1)).shape, ph_frame_pred.shape)
-        ph_frame_GHM_loss = self.ph_frame_GHM_loss_fn(
-            ph_frame_logits,
-            ph_frame_gt,
-            (mask.unsqueeze(-1) * ph_mask.unsqueeze(1)),
-            valid,
-        )
-
-        # ph_edge loss
-        # BCE_GHM loss
-        ph_edge_GHM_loss = self.ph_edge_GHM_loss_fn(
-            ph_edge_logits.unsqueeze(-1), ph_edge_gt.unsqueeze(-1), mask, valid
-        )
-
-        # EMD loss
-        ph_edge_pred = torch.nn.functional.sigmoid(ph_edge_logits.float())
-        ph_edge_EMD_loss = self.EMD_loss_fn(ph_edge_pred * mask, ph_edge_gt * mask)
-
-        # diff loss
-        ph_edge_diff_loss = self.ph_edge_diff_GHM_loss_fn(
-            (torch.diff(ph_edge_logits, 1, dim=-1) + 1).unsqueeze(-1) / 2,
-            (torch.diff(ph_edge_gt, 1, dim=-1) + 1).unsqueeze(-1) / 2,
-            mask[:, 1:],
-            valid,
-        )
-        return ph_frame_GHM_loss, ph_edge_GHM_loss, ph_edge_EMD_loss, ph_edge_diff_loss
-
-    def _get_weak_label_loss(
-        self,
-        ctc_logits,
-        ph_mask,
-        ph_seq_gt,
-        ph_seq_lengths_gt,
-        input_feature_lengths,
-        valid,
-    ):
-        ctc_logits = ctc_logits - ph_mask.unsqueeze(1).logical_not().float() * 1e9
-        log_probs_pred = nn.functional.log_softmax(ctc_logits, dim=-1)
-        # ctc loss
-        log_probs_pred = rearrange(log_probs_pred, "B T C -> T B C")
-        ctc_GHM_loss = self.CTC_GHM_loss_fn(
-            log_probs_pred,
-            ph_seq_gt,
-            input_feature_lengths,
-            ph_seq_lengths_gt,
-            valid,
-        )
-
-        return ctc_GHM_loss
-
-    def _get_consistency_loss(
-        self, ph_frame_logits, ph_edge_logits, input_feature_lengths
-    ):
-        output_tensors = torch.cat(
-            [ph_frame_logits, ph_edge_logits.unsqueeze(-1)], dim=-1
-        )
-        output_tensors = torch.nn.functional.sigmoid(output_tensors.float())
-        B = output_tensors.shape[0]
-        T = output_tensors.shape[1]
-
-        # calculate mask matrix
-        # (B//2, T, 1)
-        mask = torch.arange(T).to(self.device)
-        mask = repeat(mask, "T -> B T", B=B // 2)
-        mask = (
-            (mask < input_feature_lengths[: B // 2].unsqueeze(1))
-            .to(torch.bool)
-            .unsqueeze(-1)
-        )
-
-        # consistency loss
-        consistency_loss = self.MSE_loss_fn(
-            output_tensors[: B // 2, :, :] * mask,
-            output_tensors[B // 2 :, :, :] * mask,
-        )
-
-        return consistency_loss
-
-    def _get_pseudo_label_loss(self, ph_frame_logits, input_feature_lengths, valid):
-        B = ph_frame_logits.shape[0]
-        T = ph_frame_logits.shape[1]
-
-        ph_edge_prob = torch.nn.functional.sigmoid(ph_frame_logits.float())
-
-        pred1 = ph_edge_prob[: B // 2, :]
-        pred2 = ph_edge_prob[B // 2 :, :]
-        pseudo_label1 = (pred1 >= 0.5).float()
-        pseudo_label2 = (pred2 >= 0.5).float()
-        gradient_magnitude1 = torch.abs(pred1 - pseudo_label1)
-        gradient_magnitude2 = torch.abs(pred2 - pseudo_label2)
-        gradient_magnitude = (gradient_magnitude1 + gradient_magnitude2) / 2
-
-        # calculate mask matrix
-        # (B//2, T, 1)
-        mask = torch.arange(T).to(self.device)
-        mask = repeat(mask, "T -> B T", B=B // 2)
-        mask = (
-            (mask < input_feature_lengths[: B // 2].unsqueeze(1))
-            .to(torch.bool)
-            .unsqueeze(-1)
-        )
-        pseudo_label_mask = (  # (B//2, T)
-            mask
-            & (pseudo_label1 == pseudo_label2)
-            & (gradient_magnitude < self.pseudo_label_auto_theshold)
-        )
-
-        if pseudo_label_mask.sum() / mask.sum() < self.pseudo_label_ratio:
-            self.pseudo_label_auto_theshold += 0.005
-        else:
-            self.pseudo_label_auto_theshold -= 0.005
-
-        if pseudo_label_mask.any():
-            pseudo_label_loss = self.pseudo_label_GHM_loss_fn(
-                ph_frame_logits,
-                torch.cat([pseudo_label1, pseudo_label2], dim=0),
-                torch.cat([pseudo_label_mask, pseudo_label_mask], dim=0),
-                valid,
-            )
-        else:
-            pseudo_label_loss = torch.tensor(0).to(self.device)
-
-        return pseudo_label_loss
 
     # ==validation===========================
     def on_validation_start(self):
@@ -505,7 +228,7 @@ class LitForcedAlignmentTask(pl.LightningModule):
             ctc_logits,  # (B, T, vocab_size)
         ) = self.forward(input_feature.transpose(1, 2))
 
-        losses = self._get_loss(
+        losses = self.loss._get_loss(
             ph_frame_logits,
             ph_edge_logits,
             ctc_logits,
@@ -519,7 +242,7 @@ class LitForcedAlignmentTask(pl.LightningModule):
             valid=True,
         )
 
-        weights = self._losses_schedulers_call() * self.losses_weights
+        weights = self.loss._losses_schedulers_call() * self.loss.losses_weights
         total_loss = (torch.stack(losses) * weights).sum()
         losses.append(total_loss)
         losses = torch.stack(losses)
