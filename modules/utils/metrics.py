ff1f4e5066a4ae427a88a23450814573dfe21141
qiuqiao <690697422@qq.com>
2024-07-04 00:25:39 +0800

    add metric get_vlabeler_edit_ratio

---
+++ modules/utils/metrics.py
@@ -0,0 +1,58 @@
+from functools import lru_cache
+
+import textgrid as tg
+
+
+def get_vlabeler_edit_ratio(
+    pred: tg.PointTier, target: tg.PointTier, move_tolerance=20
+):
+    # 获得从pred编辑到target所需要的最少次数及其比例
+    # 注意这是一个略微简化的模型，不一定和vlabeler完全一致。
+    # 编辑操作包括：
+    #   插入边界
+    #   删除边界及其前一个音素（和vlabeler的操作对应）
+    #   移动边界（如果边界距离大于move_tolerance ms，就要移动）
+    #   音素替换
+
+    # vlabeler中，对TextGrid有要求，如果要满足要求的话，
+    # PointTier中的第一个和最后一个边界位置不需要编辑，最后一个音素必定为空
+    assert len(pred) >= 2 and len(target) >= 2
+    assert pred[0].time == target[0].time
+    assert target[-1].time == pred[-1].time
+    assert pred[-1].mark == "" and target[-1].mark == ""
+    max_edit_num = 2 * len(target) - 3
+
+    @lru_cache(maxsize=None)
+    def dfs(i, j):
+        # 返回将pred[:i]更改为target[:j]所需的编辑次数
+
+        # 边界条件
+        if i == 0:
+            # 一直插入边界直到j个边界，每次插入一个边界还要修改一个音素，所以是2j
+            return j * 2
+        if j == 0:
+            # 删除边界的同时会删除前方的音素，删除i次
+            return i
+
+        # case1: 插入边界，pred[:i+1]只能覆盖到target[:j]，所以要插入一个边界，和target[j+1]对应
+        # 如果和上一个音素相同，那么就无需修改音素
+        insert = dfs(i, j - 1) + 1
+        if j == 1 or target[j - 1].mark != target[j - 2].mark:
+            insert += 1
+        # case2: 删除边界，pred[:i]已经能覆盖到target[:j+1]，pred[i+1]完全无用，可以删了
+        # 这里跟vlabeler的操作是一致的，vlabeler删除一个音素会同时删除前面的边界，这里是删除边界会同时删除后一个音素
+        # 因为被删除了，所以无需修改音素
+        delete = dfs(i - 1, j) + 1
+        # case3:移动（也可以不移动）边界
+        # 如果边界距离大于boundary_move_tolerance ms，就要移动，否则不需要
+        # 如果音素不一致就要修改，否则不需要
+        move = dfs(i - 1, j - 1)
+        if abs(pred[i - 1].time - target[j - 1].time) > move_tolerance:
+            move += 1
+        if pred[i - 1].mark != target[j - 1].mark:
+            move += 1
+
+        return min(insert, delete, move)
+
+    edit_num = dfs(len(pred), len(target))
+    return edit_num / max_edit_num, edit_num
---
+++ modules/utils/metrics_test.py
@@ -0,0 +1,66 @@
+from typing import List, Tuple
+
+import textgrid as tg
+
+from modules.utils.metrics import get_vlabeler_edit_ratio
+
+
+def point_tier_from_list(list: List[Tuple[float, str]], name="") -> tg.PointTier:
+    tier = tg.PointTier(name)
+    for time, mark in list:
+        tier.add(time, mark)
+    return tier
+
+
+class Test_get_vlabeler_edit_ratio:
+    # 测试用例 1：完全相同
+    def test_same(self):
+        pred_tier = point_tier_from_list([(0, "a"), (100, "b"), (200, "")])
+        target_tier = point_tier_from_list([(0, "a"), (100, "b"), (200, "")])
+        edit_ratio, edit_num = get_vlabeler_edit_ratio(pred_tier, target_tier)
+        assert edit_ratio == 0.0
+        assert edit_num == 0
+
+    # 测试用例 2：插入边界
+    def test_insert(self):
+        pred_tier = point_tier_from_list([(0, "a"), (100, "")])
+        target_tier = point_tier_from_list([(0, "a"), (50, "a"), (100, "")])
+        edit_ratio, edit_num = get_vlabeler_edit_ratio(pred_tier, target_tier)
+        assert edit_ratio == 1 / 3
+        assert edit_num == 1
+
+        pred_tier = point_tier_from_list([(0, "a"), (100, "")])
+        target_tier = point_tier_from_list([(0, "a"), (50, "b"), (100, "")])
+        edit_ratio, edit_num = get_vlabeler_edit_ratio(pred_tier, target_tier)
+        assert edit_ratio == 2 / 3
+        assert edit_num == 2
+
+    # 测试用例 3：删除边界
+    def test_delete(self):
+        pred_tier = point_tier_from_list([(0, "a"), (50, "b"), (100, "")])
+        target_tier = point_tier_from_list([(0, "a"), (100, "")])
+        edit_ratio, edit_num = get_vlabeler_edit_ratio(pred_tier, target_tier)
+        assert edit_ratio == 1.0
+        assert edit_num == 1, f"{edit_num}!= 1"
+
+    # 测试用例 4：移动边界
+    def test_move(self):
+        pred_tier = point_tier_from_list([(0, "a"), (100, "b"), (200, "")])
+        target_tier = point_tier_from_list([(0, "a"), (121, "b"), (200, "")])
+        edit_ratio, edit_num = get_vlabeler_edit_ratio(pred_tier, target_tier)
+        assert edit_ratio == 1 / 3
+        assert edit_num == 1
+
+        pred_tier = point_tier_from_list([(0, "a"), (100, "b"), (200, "")])
+        target_tier = point_tier_from_list([(0, "a"), (120, "b"), (200, "")])
+        edit_ratio, edit_num = get_vlabeler_edit_ratio(pred_tier, target_tier)
+        assert edit_ratio == 0.0
+        assert edit_num == 0
+
+    # 测试用例 5：音素替换
+    def test_replace(self):
+        pred_tier = point_tier_from_list([(0, "a"), (100, "b"), (200, "")])
+        target_tier = point_tier_from_list([(0, "b"), (100, "c"), (200, "")])
+        edit_ratio, edit_num = get_vlabeler_edit_ratio(pred_tier, target_tier)
+        assert edit_ratio == 2 / 3
+        assert edit_num == 2
