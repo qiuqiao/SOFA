91b144b18a5bd0f38838eac5542882b5747a83ba
qiuqiao <690697422@qq.com>
2024-07-13 21:16:29 +0800

    read config through hydra

--- src/torchsofa/cli/train.py
+++ src/torchsofa/cli/train.py
@@ -1,2 +1,12 @@
-def train():
+import hydra
+from omegaconf import DictConfig, OmegaConf
+
+
+@hydra.main(version_base=None, config_path="../conf", config_name="train")
+def train(cfg: DictConfig) -> None:
     print("called train")
+    print(OmegaConf.to_yaml(cfg))
+
+
+if __name__ == "__main__":
+    train()
