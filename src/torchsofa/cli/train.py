cc3c78a1d3f8453b80dcda1b494f56390e87a091
qiuqiao <690697422@qq.com>
2024-08-05 16:21:09 +0800

    update api

--- src/torchsofa/cli/train.py
+++ src/torchsofa/cli/train.py
@@ -1,11 +1,19 @@
 import hydra
+import lightning as pl
 from omegaconf import DictConfig, OmegaConf
+
+from torchsofa.data import MixedDataModule
 
 
 @hydra.main(version_base=None, config_path="../conf", config_name="train")
 def train(cfg: DictConfig) -> None:
-    print("called train")
+    print("Training...")
+    print("config:")
     print(OmegaConf.to_yaml(cfg))
+
+    pl.seed_everything(cfg.random_seed)
+
+    datamodule = MixedDataModule(cfg.data)
 
 
 if __name__ == "__main__":
